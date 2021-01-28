447b6818b357c375fe008785acb5ccc63abc9d18
davidsavitsk <davidsavitsk@gmail.com>
2021-01-28 13:49:42 +0200

    init userRepository

---
+++ src/mycassa/src/main/java/com/savit/mycassa/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.savit.mycassa.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.savit.mycassa.entity.user.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+	Optional<User> findByEmail(String email);
+}
