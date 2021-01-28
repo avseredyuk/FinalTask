e4fcc15e9ed6c092eaff2685083d11accca8a770
davidsavitsk <davidsavitsk@gmail.com>
2021-01-28 13:50:15 +0200

    sprint boot start point

---
+++ src/mycassa/src/main/java/com/savit/mycassa/MycassaApplication.java
@@ -0,0 +1,13 @@
+package com.savit.mycassa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycassaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycassaApplication.class, args);
+	}
+
+}
